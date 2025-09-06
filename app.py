ce0706ff4e5463e7a9a6ccfd586c15d3c59e2b87
ovbenjie <benjie.edroso@oliveandvinehk.com>
2025-09-06 16:13:05 +0800

    add flask app

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
-print("hello World!")
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def hello():
+    return jsonify(message="Hello, World!")
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
